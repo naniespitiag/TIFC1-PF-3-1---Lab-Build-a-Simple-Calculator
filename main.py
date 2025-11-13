8311821eee5fa21652e4640cdaf61bdca5895e96
naniespitiag <naniespitiag@gmail.com>
2025-11-13 17:44:13 -0500

    Subiendo calculadora

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-a = int(input(''))
-b = int(input(''))
-print(a + b)
+#1.Escribe un programa que tome dos números del usuario, los sume y muestre el resultado.
+
+#2.Aqui se piden los números al usuario
+num1 = int(input("Escriba el primer numero: "))
+num2 = int(input("Escriba el segundo numero: "))
+
+# Sumamos los dos numeros que dio el usuario
+resultado = num1 + num2
+
+# Aqui se muestra el resultado
+print("El resultado de la suma es:", resultado)
